afedfbd88a463e57f8c49f6f987dcde45ed7d148
Kritika <kritika.singh1824@gmail.com>
2025-07-04 20:47:07 +0530

    Created the react +  vite app and confihured the frontend, made a basic home page

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+
+export default function Hero(){
+    return(
+         <section className="bg-green-50 min-h-[80vh] flex flex-col items-center justify-center text-center px-4">
+        <h1 className="text-4xl sm:text-5xl font-bold text-green-800 leading-tight mb-4">
+          Shop Smarter. Live Greener. 🌱
+        </h1>
+        <p className="text-green-700 text-lg max-w-xl mb-6">
+          BetterBasket helps you make eco-friendly grocery choices. Take the Eco Quiz and earn rewards for choosing greener alternatives!
+        </p>
+        <a href="/quiz">
+          <button className="bg-green-600 hover:bg-green-700 text-white px-6 py-3 rounded-full shadow-md transition">
+            Take the Eco Quiz
+          </button>
+        </a>
+      </section>
+    )
+}
---
+++ frontend/src/pages/Challenge.jsx
@@ -0,0 +1,3 @@
+export default function Challenge() {
+  return <h1 className="text-3xl font-bold text-center mt-10">Eco Quiz Page</h1>
+}
