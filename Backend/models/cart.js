fd94df099ea1180b5a41b09d98e08b3ffdd88868
Prachi Rattan <143030430+PrachiRattan@users.noreply.github.com>
2025-07-11 11:13:05 +0530

    Update cart.js

--- Backend/models/cart.js
+++ Backend/models/cart.js
@@ -1,14 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const cartSchema = new mongoose.Schema({
-  userId: { type: mongoose.Types.ObjectId, ref: "User" },
-  items: [
-    {
-      productId: { type: mongoose.Types.ObjectId, ref: "Product" },
-      quantity: { type: Number, default: 1 },
-    },
-  ],
-  createdAt: { type: Date, default: Date.now },
-});
+const cartItemSchema = new mongoose.Schema({
+productId: {
+type: String,
+required: true,
+},
+name: String,
+price: Number,
+quantity: {
+type: Number,
+default: 1,
+},
+tags: [String],
+}, { timestamps: true });
 
-module.exports = mongoose.model("Cart", cartSchema);
+module.exports = mongoose.model('CartItem', cartItemSchema);
